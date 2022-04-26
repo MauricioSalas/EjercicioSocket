5f349d6b3f1bb03e0cddc1ac2c7398c84501fe28
MauricioSalas <mauricio.salas16@inacapmail.cl>
2022-04-26 02:34:24 -0400

    Agregar archivos de proyecto.

---
+++ EjercicioCliente/EjercicioCliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCliente
+{
+    public class EjercicioCliente
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte la IP del servidor: \n");
+            string servidor = Console.ReadLine().Trim();
+            Console.WriteLine("Inserte el puerto del servidor: \n");
+            int puerto = Convert.ToInt32(Console.ReadLine().Trim());
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Conectado a Servidor {0} en Puerto: {1}", servidor, puerto);
+                string mensaje = clienteSocket.Leer();
+                Console.WriteLine(mensaje);
+                bool desconectar = false;
+                while (!desconectar)
+                {
+                    mensaje = Console.ReadLine().Trim();
+                    if (String.IsNullOrEmpty(mensaje))
+                    {
+                        desconectar = false;
+                    }
+                    else
+                    {
+                        clienteSocket.Escribir(mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            desconectar = true;
+                            clienteSocket.Desconectar();
+                        }
+                        mensaje = clienteSocket.Leer();
+                        Console.WriteLine(mensaje);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de Comunicación.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjercicioServerSocket/Program.cs
@@ -0,0 +1,68 @@
+﻿using EjercicioSocketUtils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioServerSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando servidor en puerto: {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                Console.WriteLine("Servidor iniciado!");
+                while (true)
+                {
+                    Console.WriteLine("Esperando cliente...");
+                    Socket socketCliente = servidor.ObtenerCliente();
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    bool desconectar = false;
+                    cliente.Escribir("Chat iniciado.");
+                    while (!desconectar)
+                    {
+                        string respuesta = cliente.Leer();
+                        Console.WriteLine("Cliente: {0}", respuesta);
+                        if (String.IsNullOrEmpty(respuesta))
+                        {
+                            desconectar = false;
+                        }
+                        else
+                        {
+                            if (respuesta.ToLower() == "chao")
+                            {
+                                desconectar = true;
+                            }
+                            else
+                            {
+                                respuesta = Console.ReadLine().Trim();
+                                if (respuesta.ToLower() == "chao")
+                                {
+                                    cliente.Escribir("Servidor Desconectado.");
+                                    desconectar = true;
+                                }
+                                else
+                                {
+                                    cliente.Escribir("Servidor: " + respuesta);
+                                }
+                            }
+                        }
+                    }
+                    cliente.Desconectar();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} está en uso.", puerto);
+            }
+        }
+    }
+}
